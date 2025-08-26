92d45d9dca27f44f66c0fef8489fb22fe406dfde
ezno_amd <enzoamd6@gmail.com>
2025-08-26 10:58:49 -0300

    chore: base do projeto

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# Usa esse arquivo main.py para testar as 
+# classes que tu for colocar na model
+
+# Entra na pasta src pelo terminal
+# Executa: 
+# python main.py
+# ou 
+# py main.py
+
+print('Hello World')
