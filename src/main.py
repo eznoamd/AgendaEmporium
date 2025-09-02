86d1be7ffa305579e8911efaf3c7c7b245d6025b
ezno_amd <enzoamd6@gmail.com>
2025-09-01 23:14:44 -0300

    feat: fiz a sidebar

--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
-from model.TipoModel import TipoModel
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import pyqtSlot
 
-err = TipoModel().criar_tipo(nome="Enzo", filtros="ajsdhasdhkajhsdkajdhakjshdakjsdhkjahsdkahsdjk")
-if err is not None:
-    print(err)
-else:
-    print("Tipo criado com sucesso")
+from widgets.sidebar import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.icons_widget.hide()
+        self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.button_agendamento.setChecked(True)
+
+
+def pick_style_path():
+    if sys.platform == "win32":
+        return "widgets/style.qss"
+    return "./widgets/style.qss"
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    with open(pick_style_path(), "r") as f:
+        style = f.read()
+    app.setStyleSheet(style)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
